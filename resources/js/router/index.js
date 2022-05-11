e8cecc401994f66786583ffbb33e3edac0a1d9f1
albertofanini-prog <albertofanini@outlook.com>
2022-05-11 17:48:42 +0200

    configurazione vue-router

---
+++ resources/js/router/index.js
@@ -0,0 +1,15 @@
+//File a parte per Vue-Router
+
+//importare Vue
+import Vue from 'vue'
+
+//importare componente VueRouter
+import VueRouter from 'vue-router'
+
+//indicare di usare vuerouter
+Vue.use(VueRouter)
+
+//definire delle rotte
+const router = [
+    
+]
